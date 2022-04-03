6e7e16d9a593f1480f1b75a556c3c44f2a1f100e
TomH <twhughes91159@gmail.com>
2022-04-03 19:11:43 -0400

    adding strings

--- snippy.cpp
+++ snippy.cpp
@@ -12,6 +12,7 @@ It'd be really nice to setup a template to automatically include core libraries
 */
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -40,6 +41,16 @@ namespace _nag
       // TODO > Add some stuff for the constructor to do.
       // I hate it when I forget how to simply print to the console.  It's embarassing.
       std::cout << "Hello, from Robot_Simulation_Control!\n";
+
+      string firstname;
+      cout << "What's your name? ";
+      cin >> firstname;
+
+      string lastname;
+      cout<<"Hello, "<<firstname<<" what's your last name? ";
+      cin>>lastname;
+
+      cout<<"First name length: "<<firstname.length<<"Last name size: "<<lastname.size;
    }
 
    snippy::~snippy()
